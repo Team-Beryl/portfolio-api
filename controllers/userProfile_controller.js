d92ed1022f90884e7f03c7daeef4e04e37bb6881
Felix Kojo <felixkwadwo300@gmail.com>
2024-07-12 07:53:17 +0000

    create-the-crud-user-profile-v3

--- controllers/userProfile_controller.js
+++ controllers/userProfile_controller.js
@@ -1,4 +1,5 @@
 import { UserProfileModel } from "../models/userProfile_model.js";
+import { UserModel } from "../models/user_model.js";
 import { userProfileSchema } from "../schema/user_profile_validation.js";
 
 export const postUserProfile = async (req, res, next) => {
@@ -7,11 +8,17 @@ export const postUserProfile = async (req, res, next) => {
     if (error) {
       return res.status(400).send(error.details[0].message);
     }
-    console.log("value", value);
-    const newProfile = await UserProfileModel.create({
-      ...req.body,
-      profilePicture: req?.file?.filename,
-    });
+
+    const newProfile = await UserProfileModel.create(value);
+
+    const user = await UserModel.findById(value.user);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    user.userProfile.push(newProfile.id);
+
+    await user.save();
 
     res.status(201).json({ newProfile });
   } catch (error) {
@@ -19,13 +26,14 @@ export const postUserProfile = async (req, res, next) => {
   }
 };
 
-export const getUserProfile = async (req, res, next) => {
+export const getAllUserProfile = async (req, res, next) => {
   try {
-    const allUserProfiles = await UserProfileModel.find();
-    if (allUserProfiles.length == 0) {
-      return res.status(404).send("No education added");
+    const userId = req.params.id;
+    const allUserProfile = await UserProfileModel.find({ user: userId });
+    if (allUserProfile.length == 0) {
+      return res.status(404).send("No User Profile added");
     }
-    res.status(200).json({ userProfiles: allUserProfiles });
+    res.status(200).json({ userProfiles: allUserProfile });
   } catch (error) {
     next(error);
   }
@@ -34,7 +42,7 @@ export const getUserProfile = async (req, res, next) => {
 export const getAUserProfile = async (req, res, next) => {
   try {
     const singleUserProfile = await UserProfileModel.findById(req.params.id);
-    res.json(singleUserProfile);
+    res.status(200).json(singleUserProfile);
   } catch (error) {
     next(error);
   }
@@ -47,7 +55,7 @@ export const patchUserProfile = async (req, res, next) => {
       { ...req.body, profilePicture: req?.file?.filename },
       { new: true }
     );
-    res.json(editUserProfile);
+    res.status(200).send(editUserProfile);
   } catch (error) {
     next(error);
   }
@@ -55,10 +63,8 @@ export const patchUserProfile = async (req, res, next) => {
 
 export const deleteUserProfile = async (req, res, next) => {
   try {
-    const delUserProfile = await UserProfileModel.findByIdAndDelete(
-      req.params.id
-    );
-    res.json(`User Profile with id ${req.params.id} Deleted`);
+    await UserProfileModel.findByIdAndDelete(req.params.id);
+    res.status(201).send(`User Profile with id ${req.params.id} Deleted`);
   } catch (error) {
     next(error);
   }
