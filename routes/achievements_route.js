92ae2a0748230e6920156a265cee56ad1830f2b5
Vida Owusu <vidakumiwaaowusu@gmail.com>
2024-07-17 14:06:54 +0000

    fixed errors in the achievement controller

--- routes/achievements_route.js
+++ routes/achievements_route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import {createUserAchievement, deleteUserAchievement, getAllUserAchievements, updateUserAchievement } from "../controllers/achievements_controller.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+import { remoteUpload } from "../middlewares/upload.js";
 
 export const achievementRouter = Router();
 
-achievementRouter.post('/users/achievements', isAuthenticated, createUserAchievement)
+achievementRouter.post('/users/achievements', isAuthenticated, remoteUpload.single('image'), createUserAchievement)
 
 achievementRouter.get('/users/achievements', getAllUserAchievements)
 
