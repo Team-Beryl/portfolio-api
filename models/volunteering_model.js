660e58aee459c4873f4d0191fcb9fdea175743f3
Felix Kojo <felixkwadwo300@gmail.com>
2024-07-17 10:15:09 +0000

    changes on endpoints v1

--- models/volunteering_model.js
+++ models/volunteering_model.js
@@ -5,12 +5,10 @@ const volunteeringSchema = new Schema(
   {
     organization: { type: String },
     description: { type: String },
-    skills: { type: String },
     startDate: { type: String },
     endDate: { type: String },
     location: { type: String },
     role: { type: String },
-    responsibility: { type: String },
     projectName: { type: String },
     user: { type: Types.ObjectId, ref: "User" },
   },
